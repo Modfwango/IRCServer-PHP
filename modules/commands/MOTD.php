7fd9e7d4ccf0e4da01702d9859fb4b6bbdb9f933
Clay Freeman <git@clayfreeman.com>
2014-07-04 09:01:35 -0500

    Start filling things in.

--- modules/commands/MOTD.php
+++ modules/commands/MOTD.php
@@ -9,19 +9,25 @@
 
       if ($connection->getOption("registered") == true
            && strtolower($command[0]) == "motd") {
-        if (is_string(__MOTD__) && stristr(__MOTD__, "\n")) {
-          $connection->send(":__SERVERDOMAIN__ 375 ".
+        if (is_string(__MOTD__)) {
+          $connection->send(":".__SERVERDOMAIN__." 375 ".
             $connection->getOption("nick")." :- ".__SERVERDOMAIN__.
             " Message of the Day - ");
-          foreach (explode("\n", __MOTD__) as $line) {
-            $connection->send(":__SERVERDOMAIN__ 372 ".
-              $connection->getOption("nick")." :- ".$line);
+          if (stristr(__MOTD__, "\n")) {
+            foreach (explode("\n", __MOTD__) as $line) {
+              $connection->send(":".__SERVERDOMAIN__." 372 ".
+                $connection->getOption("nick")." :- ".$line);
+            }
           }
-          $connection->send(":__SERVERDOMAIN__ 376 ".
+          else {
+            $connection->send(":".__SERVERDOMAIN__." 372 ".
+              $connection->getOption("nick")." :- ".__MOTD__);
+          }
+          $connection->send(":".__SERVERDOMAIN__." 376 ".
             $connection->getOption("nick")." :End of /MOTD command.");
         }
         else {
-          $connection->send(":__SERVERDOMAIN__ 422 ".
+          $connection->send(":".__SERVERDOMAIN__." 422 ".
             $connection->getOption("nick")." :MOTD file is missing");
         }
       }
--- modules/internal/Welcome.php
+++ modules/internal/Welcome.php
@@ -20,24 +20,22 @@
     }
 
     public function receiveUserRegistration($name, $connection) {
-      $connection->send(":".__SERVERDOMAIN__." 001 ".(
-        $connection->getOption("nick") ? $connection->getOption("nick") : "*").
-        " :Welcome to the ".__NETNAME__." Internet Relay Chat Network ".(
-        $connection->getOption("nick") ? $connection->getOption("nick") : "*"));
-      $connection->send(":".__SERVERDOMAIN__." 002 ".(
-        $connection->getOption("nick") ? $connection->getOption("nick") : "*").
-        " :Your host is ".__SERVERDOMAIN__."[".$connection->getLocalIP()."/".
-        $connection->getLocalPort."], running version ".__PROJECTVERSION__);
-      $connection->send(":".__SERVERDOMAIN__." 003 ".(
-        $connection->getOption("nick") ? $connection->getOption("nick") : "*").
-        " :This server was created ".date("D M d Y", __STARTTIME__)." at ".
-        date("H:i:s e", __STARTTIME__));
-      $connection->send(":".__SERVERDOMAIN__." 004 ".(
-        $connection->getOption("nick") ? $connection->getOption("nick") : "*").
-        " ".__SERVERDOMAIN__." ".__PROJECTVERSION__." ".__USERMODES__." ".
-        __CHANNELMODES__." ".__CHANNELMODESWITHPARAMS__);
-      $connection->send(":".__SERVERDOMAIN__." 005 ".(
-        $connection->getOption("nick") ? $connection->getOption("nick") : "*").
+      $connection->send(":".__SERVERDOMAIN__." 001 ".
+        $connection->getOption("nick")." :Welcome to the ".__NETNAME__.
+        " Internet Relay Chat Network ".$connection->getOption("nick"));
+      $connection->send(":".__SERVERDOMAIN__." 002 ".
+        $connection->getOption("nick")." :Your host is ".__SERVERDOMAIN__."[".
+        $connection->getLocalIP()."/".$connection->getLocalPort.
+        "], running version ".__PROJECTVERSION__);
+      $connection->send(":".__SERVERDOMAIN__." 003 ".
+        $connection->getOption("nick")." :This server was created ".date(
+        "D M d Y", __STARTTIME__)." at ".date("H:i:s e", __STARTTIME__));
+      $connection->send(":".__SERVERDOMAIN__." 004 ".
+        $connection->getOption("nick")." ".__SERVERDOMAIN__." ".
+        __PROJECTVERSION__." ".__USERMODES__." ".__CHANNELMODES__." ".
+        __CHANNELMODESWITHPARAMS__);
+      $connection->send(":".__SERVERDOMAIN__." 005 ".
+        $connection->getOption("nick").
         " PREFIX=() CHANTYPES= :are supported by this server");
 
       $event = EventHandling::getEventByName("commandEvent");
