09c65fe2adeb122ccbc2db2658b3bf979e693daf
Clay Freeman <git@clayfreeman.com>
2014-07-04 01:34:31 -0500

    Add the quit command.

---
+++ modules/commands/QUIT.php
@@ -0,0 +1,32 @@
+<?php
+  class @@CLASSNAME@@ {
+    public $depend = array("CommandEvent");
+    public $name = "MOTD";
+
+    public function receiveCommand($name, $data) {
+      $connection = $data[0];
+      $command = $data[1];
+
+      if (strtolower($command[0]) == "quit") {
+        $message = "Client Quit";
+        if (count($command) > 1) {
+          $message = "Quit: ".$command[1];
+        }
+
+        if ($connection->getOption("registered") == true) {
+          $connection->send(":".$connection->getOption("nick")."!".
+            $connection->getOption("ident")."@".$connection->getHost().
+            " QUIT :".$message);
+        }
+        $connection->send("ERROR :Closing Link: ".$connection->getHost()." (".
+          $message.")");
+        $connection->disconnect();
+      }
+    }
+
+    public function isInstantiated() {
+      EventHandling::registerForEvent("commandEvent", $this, "receiveCommand");
+      return true;
+    }
+  }
+?>
