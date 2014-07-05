a3c33fcee2d506b5ebc0f428cea70a17f724ee55
Clay Freeman <git@clayfreeman.com>
2014-07-04 23:38:56 -0500

    Add NAMES command.

--- TODO.md
+++ TODO.md
@@ -3,4 +3,6 @@ Make PONG verify subject
 Implement Channel module to store channel states
 Check max targets for PRIVMSG
 Check max length for PRIVMSG
-Return true even if user not registered on commandEvent based modules.
+Return true even if user not registered on commandEvent based modules
+Consider moving Welcome triggers of MOTD and LUSERS
+Think of a creative way to preprocess NAMES with mode prefixes
---
+++ modules/commands/NAMES.php
@@ -0,0 +1,65 @@
+<?php
+  class @@CLASSNAME@@ {
+    public $depend = array("Channel", "CommandEvent");
+    public $name = "NAMES";
+
+    public function receiveCommand($name, $data) {
+      $connection = $data[0];
+      $command = $data[1];
+
+      if ($connection->getOption("registered") == true
+           && strtolower($command[0]) == "names") {
+        if (count($command) > 1) {
+          $channels = array($command[1]);
+          if (stristr($command[1], ",")) {
+            $channels = explode(",", $command[1]);
+          }
+          foreach ($channels as $channel) {
+            $channel = ModuleManagement::getModuleByName("Channel")->
+              getChannelByName($channel);
+            if ($channel != false) {
+              $members = array();
+              foreach ($channel["members"] as $id) {
+                foreach (ConnectionManagement::getConnections() as $c) {
+                  if ($c->getOption("id") == $id) {
+                    $members[] = $c->getOption("nick");
+                  }
+                }
+              }
+
+              $base = ":".__SERVERDOMAIN__." 353 ".
+                $connection->getOption("nick")." = ".$channel." :";
+              $remaining = (510 - strlen($base));
+              foreach ($members as $member) {
+                $remaining -= (strlen($member) + 1);
+                if ($remaining > -1) {
+                  if (!isset($items)) {
+                    $items = array();
+                  }
+                  $items[] = $member;
+                }
+                else {
+                  $remaining = (510 - strlen($base));
+                  $connection->send($base.implode(" ", $line));
+                }
+              }
+            }
+          }
+          if (count($channels) == 1) {
+            $connection->send(":".__SERVERDOMAIN__." 366 ".
+              $connection->getOption("nick")." ".$channels[0]." :End of /NAMES list.");
+          }
+        }
+        $connection->send(":".__SERVERDOMAIN__." 366 ".
+          $connection->getOption("nick")." * :End of /NAMES list.");
+        return true;
+      }
+      return false;
+    }
+
+    public function isInstantiated() {
+      EventHandling::registerForEvent("commandEvent", $this, "receiveCommand");
+      return true;
+    }
+  }
+?>
