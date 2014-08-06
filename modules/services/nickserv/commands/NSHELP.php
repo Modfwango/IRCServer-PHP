802efaa44e6d38ec0b4486e39b31c6c64ebd09b3
Clay Freeman <git@clayfreeman.com>
2014-08-05 20:06:40 -0500

    Change line limit to 64

--- modules/services/nickserv/commands/NSHELP.php
+++ modules/services/nickserv/commands/NSHELP.php
@@ -22,11 +22,11 @@
         sort($commands);
         $lines = array();
         while (count($commands) > 0) {
-          if (strlen($commands[0]) > 95) {
+          if (strlen($commands[0]) > 63) {
             array_shift($commands);
           }
           $curLine = null;
-          while (isset($commands[0]) && strlen($curLine." ".$commands[0]) < 97) {
+          while (isset($commands[0]) && strlen($curLine." ".$commands[0]) < 65) {
             $curLine .= " ".strtoupper(array_shift($commands));
           }
           if ($curLine != null) {
@@ -34,8 +34,8 @@
           }
         }
         $title = "| List of NickServ Commands |";
-        $message .= "|".str_repeat("=", floor((94 - strlen($title)) / 2)).
-          $title.str_repeat("=", ceil((94 - strlen($title)) / 2))."|\n";
+        $message .= "|".str_repeat("=", floor((64 - strlen($title)) / 2)).
+          $title.str_repeat("=", ceil((64 - strlen($title)) / 2))."|\n";
         $message .= implode("\n", $lines);
         $lines = explode("\n", $message);
         foreach ($lines as $line) {
