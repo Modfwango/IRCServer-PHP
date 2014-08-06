42c7ddfb55e61e521a6b3b94eb017528c61ac1cf
Clay Freeman <git@clayfreeman.com>
2014-08-06 00:02:58 -0500

    UGH

--- modules/services/Util.php
+++ modules/services/Util.php
@@ -12,8 +12,8 @@
       Logger::info(var_export($s, true));
       $line = 0;
       foreach ($s as $string) {
-        Logger::info(var_export($s, true));
         while (count($string) > 0) {
+          Logger::info(var_export($s, true));
           $line++;
           $ret[$line] = null;
           $lastCount = count($string);
@@ -22,7 +22,7 @@
             $ret[$line] .= " ".array_shift($string);
             $ret[$line] = trim($ret[$line]);
           }
-          /*if (count($string) == $lastCount) {
+          if (count($string) == $lastCount) {
             if (strlen($string[0]) > ($size - strlen($ending))) {
               $str = chunk_split(array_shift($string),
                 ($size - (strlen($ending) + 1)), "-".$ending);
@@ -30,7 +30,7 @@
                 array_unshift($string, $substr);
               }
             }
-          }*/
+          }
         }
       }
       Logger::info(var_export($ret, true));
