f231aeba142fa7626e35a787b5226e93b9e4d43e
Clay Freeman <git@clayfreeman.com>
2014-08-12 20:16:51 -0500

    Start the ever-long process of making and debugging the ban mode.

---
+++ modules/modes/ChannelBan.php
@@ -0,0 +1,60 @@
+<?php
+  class @@CLASSNAME@@ {
+    public $depend = array("Channel", "Client", "ChannelModeEvent", "Modes");
+    public $name = "ChannelBan";
+    private $channel = null;
+    private $client = null;
+    private $modes = null;
+
+    public function receiveChannelMode($name, $id, $data) {
+      $source = $data[0];
+      $channel = $data[1];
+      $modes = $data[2];
+
+      $h = array();
+      $has = $this->channel->hasModes($channel["name"],
+        array("ChannelBan"));
+      if (is_array($has) && count($has) > 0) {
+        foreach ($has as $m) {
+          $h[strtolower($m["param"])] = true;
+        }
+      }
+      foreach ($modes as $key => $mode) {
+        $mode["param"] = $this->client->getPrettyMask($mode["param"]);
+        if ($mode["name"] == "ChannelBan") {
+          if (!isset($h[strtolower($mode["param"])])) {
+            $h[strtolower($mode["param"])] = false;
+          }
+          if ($mode["operation"] == "+") {
+            if ($h[strtolower($mode["param"])] != false) {
+              unset($modes[$key]);
+            }
+            else {
+              $h[strtolower($mode["param"])] = true;
+            }
+          }
+          if ($mode["operation"] == "-") {
+            if ($h[strtolower($mode["param"])] == false) {
+              unset($modes[$key]);
+            }
+            else {
+              $h[strtolower($mode["param"])] = false;
+            }
+          }
+        }
+      }
+      $data[2] = $modes;
+      return array(null, $data);
+    }
+
+    public function isInstantiated() {
+      $this->channel = ModuleManagement::getModuleByName("Channel");
+      $this->client = ModuleManagement::getModuleByName("Client");
+      $this->modes = ModuleManagement::getModuleByName("Modes");
+      $this->modes->setMode(array("ChannelBan", "b", "0", "3"));
+      EventHandling::registerAsEventPreprocessor("channelModeEvent", $this,
+        "receiveChannelMode");
+      return true;
+    }
+  }
+?>
