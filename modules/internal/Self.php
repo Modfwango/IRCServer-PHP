b466a4d068e6b352bc4ba2119214618d28c4bf75
Clay Freeman <git@clayfreeman.com>
2014-08-16 19:20:21 -0500

    Make the MOTD rehashable.

--- modules/internal/Self.php
+++ modules/internal/Self.php
@@ -12,30 +12,36 @@
     }
 
     public function loadConfig($name = null, $data = null) {
-      $config = @json_decode(trim(StorageHandling::loadFile($this,
-        "config.json")), true);
       $motd = StorageHandling::loadFile($this, "motd.txt");
       if ($motd == false) {
         $motd = "This is a test!";
         StorageHandling::saveFile($this, "motd.txt", $motd);
       }
-      if (!is_array($config)) {
-        $config = array(
-          "motd" => $motd,
-          "netname" => "PHPNet",
-          "pingtime" => 120,
-          "version" => "IRCServer-PHP-dev",
-          "serverdomain" => "home.clayfreeman.com",
-          "serverdescription" => "Oh, look; a server!"
-        );
-        StorageHandling::saveFile($this, "config.json", json_encode($config,
-          JSON_PRETTY_PRINT));
+      if (count($this->config) == 0) {
+        $config = @json_decode(trim(StorageHandling::loadFile($this,
+          "config.json")), true);
+        if (!is_array($config)) {
+          $config = array(
+            "motd" => $motd,
+            "netname" => "PHPNet",
+            "pingtime" => 120,
+            "version" => "IRCServer-PHP-dev",
+            "serverdomain" => "home.clayfreeman.com",
+            "serverdescription" => "Oh, look; a server!"
+          );
+          StorageHandling::saveFile($this, "config.json", json_encode($config,
+            JSON_PRETTY_PRINT));
+        }
+        $this->config = $config;
       }
-      $this->config = $config;
+      else {
+        $this->config["motd"] = $motd;
+      }
     }
 
     public function isInstantiated() {
       $this->loadConfig();
+      EventHandling::registerForEvent("rehashEvent", $this, "loadConfig");
       return true;
     }
   }
