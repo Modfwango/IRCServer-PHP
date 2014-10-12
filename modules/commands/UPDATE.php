9601dea953cb3a402fd43db2a2f074627b533a5d
Clay Freeman <git@clayfreeman.com>
2014-10-11 19:48:46 -0500

    Add /UPDATE macro for "/EVAL return shell_exec..."

---
+++ modules/commands/UPDATE.php
@@ -0,0 +1,58 @@
+<?php
+  class __CLASSNAME__ {
+    public $depend = array("CommandEvent", "Numeric", "Self");
+    public $name = "UPDATE";
+    private $numeric = null;
+    private $self = null;
+
+    public function receiveCommand($name, $data) {
+      $connection = $data[0];
+      $command = $data[1];
+
+      foreach ($command as $key => $param) {
+        if (trim($param) == null) {
+          unset($command[$key]);
+        }
+      }
+      $command = array_values($command);
+
+      if ($connection->getOption("registered") == true) {
+        if ($connection->getOption("operator") != false) {
+          $event = EventHandling::getEventByName("commandEvent");
+          if ($event != false) {
+            foreach ($event[2] as $id => $registration) {
+              // Trigger the commandEvent event for each registered module.
+              if (strtolower(trim($registration[2])) == "eval") {
+                EventHandling::triggerEvent("commandEvent", $id,
+                  array($connection, explode(" ",
+                  "return shell_exec('sh update.sh');")));
+              }
+            }
+          }
+        }
+        else {
+          $connection->send($this->numeric->get("ERR_NOPRIVILEGES", array(
+            $this->self->getConfigFlag("serverdomain"),
+            ($connection->getOption("nick") ?
+            $connection->getOption("nick") : "*")
+          )));
+        }
+      }
+      else {
+        $connection->send($this->numeric->get("ERR_NOTREGISTERED", array(
+          $this->self->getConfigFlag("serverdomain"),
+          ($connection->getOption("nick") ?
+          $connection->getOption("nick") : "*")
+        )));
+      }
+    }
+
+    public function isInstantiated() {
+      $this->numeric = ModuleManagement::getModuleByName("Numeric");
+      $this->self = ModuleManagement::getModuleByName("Self");
+      EventHandling::registerForEvent("commandEvent", $this, "receiveCommand",
+        "update");
+      return true;
+    }
+  }
+?>
