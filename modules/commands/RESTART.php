0d51d0b20bdeb026f68b15a04f556a1d34f890ff
Clay Freeman <git@clayfreeman.com>
2014-10-11 20:06:59 -0500

    Add the RESTART command.

---
+++ modules/commands/RESTART.php
@@ -0,0 +1,49 @@
+<?php
+  class __CLASSNAME__ {
+    public $depend = array("CommandEvent", "Numeric", "Self");
+    public $name = "RESTART";
+    private $numeric = null;
+    private $self = null;
+
+    public function receiveCommand($name, $data) {
+      $connection = $data[0];
+      $command = $data[1];
+
+      foreach ($command as $key => $param) {
+        if (trim($param) == null) {
+          unset($command[$key]);
+        }
+      }
+      $command = array_values($command);
+
+      if ($connection->getOption("registered") == true) {
+        if ($connection->getOption("operator") != false) {
+          exec("screen -dm php ".escapeshellarg(__PROJECTROOT__."/main.php"));
+          getMain()->shutdown();
+        }
+        else {
+          $connection->send($this->numeric->get("ERR_NOPRIVILEGES", array(
+            $this->self->getConfigFlag("serverdomain"),
+            ($connection->getOption("nick") ?
+            $connection->getOption("nick") : "*")
+          )));
+        }
+      }
+      else {
+        $connection->send($this->numeric->get("ERR_NOTREGISTERED", array(
+          $this->self->getConfigFlag("serverdomain"),
+          ($connection->getOption("nick") ?
+          $connection->getOption("nick") : "*")
+        )));
+      }
+    }
+
+    public function isInstantiated() {
+      $this->numeric = ModuleManagement::getModuleByName("Numeric");
+      $this->self = ModuleManagement::getModuleByName("Self");
+      EventHandling::registerForEvent("commandEvent", $this, "receiveCommand",
+        "restart");
+      return true;
+    }
+  }
+?>
