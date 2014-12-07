f9fc63170acb448eaf2390fd2a2c5f503a96b34a
Clay Freeman <git@clayfreeman.com>
2014-12-07 17:53:40 -0600

    Attempt to finalize link initialization routine.

---
+++ modules/protocol/juno/PASS~juno.php
@@ -0,0 +1,39 @@
+<?php
+  class __CLASSNAME__ {
+    public $depend = array("Outgoing~juno");
+    public $name = "PASS~juno";
+    private $juno = null;
+
+    public function receiveCommand($name, $data) {
+      $connection = $data[0];
+      $command = $data[1];
+
+      foreach ($command as $key => $param) {
+        if (trim($param) == null) {
+          unset($command[$key]);
+        }
+      }
+      $command = array_values($command);
+
+      if ($connection->getOption("recvpass") == $command[0]) {
+        $connection->setOption("authenticated", true);
+        if ($connection->getOption("sentpass") == true) {
+          $connection->send("READY");
+        }
+        else {
+          $this->juno->authenticate($connection);
+        }
+      }
+      else {
+        $connection->disconnect();
+      }
+    }
+
+    public function isInstantiated() {
+      $this->juno = ModuleManagement::getModuleByName("Outgoing~juno");
+      EventHandling::registerForEvent("commandEvent", $this, "receiveCommand",
+        array("pass", true, "juno"));
+      return true;
+    }
+  }
+?>
