aba796222bf023b689de0ab5219be2dd380cfc2b
Clay Freeman <git@clayfreeman.com>
2014-12-18 19:57:45 -0600

    Remove prefix and weight from ACM status modes.

--- modules/protocol/juno/commands/ACM~juno.php
+++ modules/protocol/juno/commands/ACM~juno.php
@@ -20,14 +20,8 @@
       if (count($command) > 0) {
         foreach ($command as $item) {
           $item = explode(":", $item);
-          if ($item[2] == "4") {
-            $this->modes->setMode(array($item[0], $item[1], "0", $item[2],
-              $item[3], $item[4]), $connection->getOption("alphabet"));
-          }
-          else {
-            $this->modes->setMode(array($item[0], $item[1], "0", $item[2]),
-              $connection->getOption("alphabet"));
-          }
+          $this->modes->setMode(array($item[0], $item[1], "0", $item[2]),
+            $connection->getOption("alphabet"));
         }
       }
     }
@@ -36,13 +30,7 @@
       $lburst = $connection->getOption("lburst");
       $modes = array();
       foreach ($this->modes->getModesByTarget("0") as $mode) {
-        if ($mode[3] == "4") {
-          $modes[] = $mode[0].":".$mode[1].":".$mode[3].":".$mode[4].":".
-            $mode[5];
-        }
-        else {
-          $modes[] = $mode[0].":".$mode[1].":".$mode[3];
-        }
+        $modes[] = $mode[0].":".$mode[1].":".$mode[3];
       }
       $lburst[] = ":".$this->juno->getSID()." ACM ".implode(" ", $modes);
       $connection->setOption("lburst", $lburst);
